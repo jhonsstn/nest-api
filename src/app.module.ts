70554a61ae36335640e985a207685997ae2f22d9
Jhon Santana <jhonsstn@gmail.com>
2022-11-16 15:39:54 -0300

    feat: setup db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './models/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'db',
+      autoLoadEntities: true,
+      synchronize: true, // Disable in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
