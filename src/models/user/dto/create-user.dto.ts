6a4f7da95dba32fb7ebf382b9f7d76d5234ebdaa
Jhon Santana <jhonsstn@gmail.com>
2022-11-16 19:31:16 -0300

    feat: add create user dto data validation

--- src/models/user/dto/create-user.dto.ts
+++ src/models/user/dto/create-user.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
   readonly username: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(/(?=.*[a-z])/, {
+    message: 'password must contain at least one lowercase character',
+  })
+  @Matches(/(?=.*[A-Z])/, {
+    message: 'password must have one uppercase character',
+  })
   readonly password: string;
 }
