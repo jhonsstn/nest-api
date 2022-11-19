d044a03e61d7164ab6bd4c107d7aaec8d7b5f9d0
Jhon Santana <jhonsstn@gmail.com>
2022-11-19 19:07:10 -0300

    feat: add schema validation to config module env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as joi from 'joi';
 import { AuthModule } from './auth/auth.module';
 import { HasherModule } from './common/hasher/hasher.module';
 import { AccountModule } from './models/account/account.module';
@@ -9,7 +10,15 @@ import { UserModule } from './models/user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: joi.object({
+        DB_HOST: joi.string().required(),
+        DB_PORT: joi.number().required().default(5432),
+        DB_USERNAME: joi.string().required(),
+        DB_PASSWORD: joi.string().required(),
+        DB_NAME: joi.string().required(),
+      }),
+    }),
     UserModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
