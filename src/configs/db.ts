feac6206acc4442ade14bf03542c0ceff92d4639
Jhon Santana <jhonsstn@gmail.com>
2022-11-21 15:44:01 -0300

    fix: typeorm synchronization option

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -19,16 +19,6 @@ const dbEnvironment = {
     autoLoadEntities: true,
     synchronize: true,
   },
-  production: {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    autoLoadEntities: true,
-    synchronize: false,
-  },
 };
 
 export default dbEnvironment;
